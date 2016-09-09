9c87c85be2e21f382fa9b42b71ca3151a6ba08f2
dim <web.best.master@gmail.com>
2016-09-09 22:50:09 +0300

    done

--- www/js/bee.js
+++ www/js/bee.js
@@ -3,10 +3,10 @@ var config = {
     size: {
         min: 20,
         max: 60,
-        default: 20
+        default: 60
     },
     round: 5,
-    margin: 2
+    margin: 1
 
 };
 
@@ -15,7 +15,6 @@ var gsap = require('gsap');
 export default class Bee {
 
     constructor(game, wrapper, endX, endY) {
-
 
         config.size.default = (game.world.width / wrapper.getWidth() * 0.9) | 0;
 
@@ -43,16 +42,13 @@ export default class Bee {
         graphics.drawRoundedRect(margin, margin, defaultSize - margin * 2, defaultSize - margin * 2, config.round);
         graphics.endFill();
 
-        graphics.beginFill(0x000000);
+        graphics.beginFill(0xCC0000);
         margin *= 2;
         graphics.drawRoundedRect(margin, margin, defaultSize - margin * 2, defaultSize - margin * 2, config.round);
         graphics.endFill();
 
-
-        // graphics.pivot.x = defaultSize / 2;
-        // graphics.pivot.y = defaultSize / 2;
-        // graphics.anchor.x = 0.5;
-        // graphics.anchor.y = 0.5;
+        graphics.pivot.set(defaultSize / 2, defaultSize / 2);
+        graphics.anchor.set(0.5, 0.5);
         graphics.rotation = Math.PI / 4;
 
         graphics.position.x = Math.random() * game.world.width;
@@ -69,12 +65,17 @@ export default class Bee {
         var bee = this,
             graphics = bee.graphics;
 
-        var lt = new gsap.TimelineMax({onComplete: () => bee.moveToEnd() });
+        var lt = new gsap.TimelineMax({onComplete: () => bee.moveToEnd()});
 
         bee.tweens.start = lt;
 
         lt
-            .to(graphics.position, 10, {x: bee.startX, y: bee.startY, ease: gsap.Bounce.easeOut, delay: Math.random() * 3})
+            .to(graphics.position, 10, {
+                x: bee.startX,
+                y: bee.startY,
+                ease: gsap.Bounce.easeOut,
+                delay: Math.random() * 3
+            })
             .to(graphics, 2, {alpha: 1, delay: Math.random() * 2}, 0);
 
 
@@ -86,11 +87,16 @@ export default class Bee {
             defaultSize = config.size.default,
             graphics = bee.graphics;
 
-        bee.tweens.end = gsap.TweenMax.to(graphics.position, 4, {
-            x: bee.endX * defaultSize + bee.startX + config.size.default / 2,
-            y: bee.endY * defaultSize + bee.startY + config.size.default / 2,
-            ease: gsap.Bounce.easeOut
-        });
+        var lt = new gsap.TimelineMax();
+
+        lt
+            .to(graphics.position, 4, {
+                x: bee.endX * defaultSize + bee.startX + config.size.default / 2,
+                y: bee.endY * defaultSize + bee.startY + config.size.default / 2,
+                ease: gsap.Elastic.easeOut.config(1, 0.3)
+            })
+            .to(graphics, 8, { rotation: Math.PI * 4.25 * (Math.random() > 0.5 ? -1 : 1), ease: gsap.Power3.easeOut }, 0)
+
 
     }
 
--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,15 @@
+window.addEventListener('resize', () => location.reload(), false);
+
 var json = require('json/data');
 
-var game = new Phaser.Game(document.documentElement.clientWidth, document.documentElement.clientHeight, Phaser.AUTO, '');
+var game = new Phaser.Game(
+    document.documentElement.clientWidth ,
+    document.documentElement.clientHeight ,
+    Phaser.AUTO,
+    ''
+);
+
+game.resolution = window.devicePixelRatio || 1;
 
 import BeeWrapper from 'bee-wrapper';
 
@@ -12,14 +21,25 @@ var mainState = {
 
     create: function () {
 
+        // var graphics = game.add.graphics();
+
+        // graphics.beginFill(0xcc00cc);
+        // graphics.drawRoundedRect(10, 10, 200, 200, 5);
+        // graphics.endFill();
 
         // game.world.setBounds(0, 0, document.documentElement.clientWidth, document.documentElement.clientHeight);
 
         var wrapper = new BeeWrapper(game, json);
 
+
+
         // game.world.camera.scale.set(.5);
 
         // game.world.camera.setPosition(400, 400);
+/*
+        game.canvas.style.width = document.documentElement.clientWidth + 'px';
+        game.canvas.style.height = document.documentElement.clientHeight + 'px';
+*/
 
 
     },
